918c18a13157dcfd108a5bb193505f7e500a44d0
Emir Muhammadzadeh <amir.msc90@gmail.com>
2025-12-24 09:48:39 +0330

    The public auth api extended and other features added

--- apps/backend/src/public-api/routes/v1/public.integrations.controller.ts
+++ apps/backend/src/public-api/routes/v1/public.integrations.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
   Query,
   UploadedFile,
+  UseFilters,
   UseInterceptors,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
@@ -31,6 +32,18 @@ import axios from 'axios';
 import { Readable } from 'stream';
 import { lookup } from 'mime-types';
 import * as Sentry from '@sentry/nestjs';
+import { ioRedis } from '@gitroom/nestjs-libraries/redis/redis.service';
+import { ConnectIntegrationDto } from '@gitroom/nestjs-libraries/dtos/integrations/connect.integration.dto';
+import { IntegrationManager } from '@gitroom/nestjs-libraries/integrations/integration.manager';
+import { IntegrationFunctionDto } from '@gitroom/nestjs-libraries/dtos/integrations/integration.function.dto';
+import { IntegrationTimeDto } from '@gitroom/nestjs-libraries/dtos/integrations/integration.time.dto';
+import { AuthService } from '@gitroom/helpers/auth/auth.service';
+import { AuthTokenDetails } from '@gitroom/nestjs-libraries/integrations/social/social.integrations.interface';
+import { NotEnoughScopes, RefreshToken } from '@gitroom/nestjs-libraries/integrations/social.abstract';
+import { NotEnoughScopesFilter } from '@gitroom/nestjs-libraries/integrations/integration.missing.scopes';
+import { pricing } from '@gitroom/nestjs-libraries/database/prisma/subscriptions/pricing';
+import { RefreshIntegrationService } from '@gitroom/nestjs-libraries/integrations/refresh.integration.service';
+import { timer } from '@gitroom/helpers/utils/timer';
 
 @ApiTags('Public API')
 @Controller('/public/v1')
@@ -40,7 +53,9 @@ export class PublicIntegrationsController {
   constructor(
     private _integrationService: IntegrationService,
     private _postsService: PostsService,
-    private _mediaService: MediaService
+    private _mediaService: MediaService,
+    private _integrationManager: IntegrationManager,
+    private _refreshIntegrationService: RefreshIntegrationService
   ) {}
 
   @Post('/upload')
@@ -188,5 +203,329 @@ export class PublicIntegrationsController {
       body.functionName,
       body.params
     );
+  }
+
+  @Get('/social/:integration')
+  @CheckPolicies([AuthorizationActions.Create, Sections.CHANNEL])
+  async getIntegrationUrl(
+    @Param('integration') integration: string,
+    @Query('refresh') refresh: string,
+    @Query('externalUrl') externalUrl: string
+  ) {
+    Sentry.metrics.count("public_api-request", 1);
+    if (
+      !this._integrationManager
+        .getAllowedSocialsIntegrations()
+        .includes(integration)
+    ) {
+      throw new Error('Integration not allowed');
+    }
+
+    const integrationProvider =
+      this._integrationManager.getSocialIntegration(integration);
+
+    if (integrationProvider.externalUrl && !externalUrl) {
+      throw new Error('Missing external url');
+    }
+
+    try {
+      const getExternalUrl = integrationProvider.externalUrl
+        ? {
+            ...(await integrationProvider.externalUrl(externalUrl)),
+            instanceUrl: externalUrl,
+          }
+        : undefined;
+
+      const { codeVerifier, state, url } =
+        await integrationProvider.generateAuthUrl(getExternalUrl);
+
+      if (refresh) {
+        await ioRedis.set(`refresh:${state}`, refresh, 'EX', 300);
+      }
+
+      await ioRedis.set(`login:${state}`, codeVerifier, 'EX', 300);
+      await ioRedis.set(
+        `external:${state}`,
+        JSON.stringify(getExternalUrl),
+        'EX',
+        300
+      );
+
+      return { url };
+    } catch (err) {
+      return { err: true };
+    }
+  }
+
+  @Post('/social/:integration/connect')
+  @CheckPolicies([AuthorizationActions.Create, Sections.CHANNEL])
+  @UseFilters(new NotEnoughScopesFilter())
+  async connectSocialMedia(
+    @GetOrgFromRequest() org: Organization,
+    @Param('integration') integration: string,
+    @Body() body: ConnectIntegrationDto
+  ) {
+    Sentry.metrics.count("public_api-request", 1);
+    if (
+      !this._integrationManager
+        .getAllowedSocialsIntegrations()
+        .includes(integration)
+    ) {
+      throw new Error('Integration not allowed');
+    }
+
+    const integrationProvider =
+      this._integrationManager.getSocialIntegration(integration);
+
+    const getCodeVerifier = integrationProvider.customFields
+      ? 'none'
+      : await ioRedis.get(`login:${body.state}`);
+    if (!getCodeVerifier) {
+      throw new Error('Invalid state');
+    }
+
+    if (!integrationProvider.customFields) {
+      await ioRedis.del(`login:${body.state}`);
+    }
+
+    const details = integrationProvider.externalUrl
+      ? await ioRedis.get(`external:${body.state}`)
+      : undefined;
+
+    if (details) {
+      await ioRedis.del(`external:${body.state}`);
+    }
+
+    const refresh = await ioRedis.get(`refresh:${body.state}`);
+    if (refresh) {
+      await ioRedis.del(`refresh:${body.state}`);
+    }
+
+    const {
+      error,
+      accessToken,
+      expiresIn,
+      refreshToken,
+      id,
+      name,
+      picture,
+      username,
+      additionalSettings,
+      // eslint-disable-next-line no-async-promise-executor
+    } = await new Promise<AuthTokenDetails>(async (res) => {
+      const auth = await integrationProvider.authenticate(
+        {
+          code: body.code,
+          codeVerifier: getCodeVerifier,
+          refresh: body.refresh,
+        },
+        details ? JSON.parse(details) : undefined
+      );
+
+      if (typeof auth === 'string') {
+        return res({
+          error: auth,
+          accessToken: '',
+          id: '',
+          name: '',
+          picture: '',
+          username: '',
+          additionalSettings: [],
+        });
+      }
+
+      if (refresh && integrationProvider.reConnect) {
+        const newAuth = await integrationProvider.reConnect(
+          auth.id,
+          refresh,
+          auth.accessToken
+        );
+        return res({ ...newAuth, refreshToken: body.refresh });
+      }
+
+      return res(auth);
+    });
+
+    if (error) {
+      throw new NotEnoughScopes(error);
+    }
+
+    if (!id) {
+      throw new NotEnoughScopes('Invalid API key');
+    }
+
+    if (refresh && String(id) !== String(refresh)) {
+      throw new NotEnoughScopes(
+        'Please refresh the channel that needs to be refreshed'
+      );
+    }
+
+    let validName = name;
+    if (!validName) {
+      if (username) {
+        validName = username.split('.')[0] ?? username;
+      } else {
+        validName = `Channel_${String(id).slice(0, 8)}`;
+      }
+    }
+
+    if (
+      process.env.STRIPE_PUBLISHABLE_KEY &&
+      org.isTrailing &&
+      (await this._integrationService.checkPreviousConnections(
+        org.id,
+        String(id)
+      ))
+    ) {
+      throw new HttpException('', 412);
+    }
+
+    return this._integrationService.createOrUpdateIntegration(
+      additionalSettings,
+      !!integrationProvider.oneTimeToken,
+      org.id,
+      validName.trim(),
+      picture,
+      'social',
+      String(id),
+      integration,
+      accessToken,
+      refreshToken,
+      expiresIn,
+      username,
+      refresh ? false : integrationProvider.isBetweenSteps,
+      body.refresh,
+      +body.timezone,
+      details
+        ? AuthService.fixedEncryption(details)
+        : integrationProvider.customFields
+        ? AuthService.fixedEncryption(
+            Buffer.from(body.code, 'base64').toString()
+          )
+        : undefined
+    );
+  }
+
+  @Post('/provider/:id/connect')
+  async saveProviderPage(
+    @Param('id') id: string,
+    @Body() body: any,
+    @GetOrgFromRequest() org: Organization
+  ) {
+    Sentry.metrics.count("public_api-request", 1);
+    return this._integrationService.saveProviderPage(org.id, id, body);
+  }
+
+  @Delete('/integrations/:id')
+  async deleteChannel(
+    @GetOrgFromRequest() org: Organization,
+    @Param('id') id: string
+  ) {
+    Sentry.metrics.count("public_api-request", 1);
+    const isTherePosts = await this._integrationService.getPostsForChannel(
+      org.id,
+      id
+    );
+    if (isTherePosts.length) {
+      for (const post of isTherePosts) {
+        await this._postsService.deletePost(org.id, post.group);
+      }
+    }
+
+    return this._integrationService.deleteChannel(org.id, id);
+  }
+
+  @Post('/integrations/:id/disable')
+  disableChannel(
+    @GetOrgFromRequest() org: Organization,
+    @Param('id') id: string
+  ) {
+    Sentry.metrics.count("public_api-request", 1);
+    return this._integrationService.disableChannel(org.id, id);
+  }
+
+  @Post('/integrations/:id/enable')
+  enableChannel(
+    @GetOrgFromRequest() org: Organization,
+    @Param('id') id: string
+  ) {
+    Sentry.metrics.count("public_api-request", 1);
+    return this._integrationService.enableChannel(
+      org.id,
+      // @ts-ignore
+      org?.subscription?.totalChannels || pricing.FREE.channel,
+      id
+    );
+  }
+
+  @Post('/integrations/:id/time')
+  async setTime(
+    @GetOrgFromRequest() org: Organization,
+    @Param('id') id: string,
+    @Body() body: IntegrationTimeDto
+  ) {
+    Sentry.metrics.count("public_api-request", 1);
+    return this._integrationService.setTimes(org.id, id, body);
+  }
+
+  @Post('/function')
+  async functionIntegration(
+    @GetOrgFromRequest() org: Organization,
+    @Body() body: IntegrationFunctionDto
+  ): Promise<any> {
+    Sentry.metrics.count("public_api-request", 1);
+    const getIntegration = await this._integrationService.getIntegrationById(
+      org.id,
+      body.id
+    );
+    if (!getIntegration) {
+      throw new Error('Invalid integration');
+    }
+
+    const integrationProvider = this._integrationManager.getSocialIntegration(
+      getIntegration.providerIdentifier
+    );
+    if (!integrationProvider) {
+      throw new Error('Invalid provider');
+    }
+
+    // @ts-ignore
+    if (integrationProvider[body.name]) {
+      try {
+        // @ts-ignore
+        const load = await integrationProvider[body.name](
+          getIntegration.token,
+          body.data,
+          getIntegration.internalId,
+          getIntegration
+        );
+
+        return load;
+      } catch (err) {
+        if (err instanceof RefreshToken) {
+          const data = await this._refreshIntegrationService.refresh(
+            getIntegration
+          );
+
+          if (!data) {
+            return;
+          }
+
+          const { accessToken } = data;
+
+          if (accessToken) {
+            if (integrationProvider.refreshWait) {
+              await timer(10000);
+            }
+            return this.functionIntegration(org, body);
+          }
+
+          return false;
+        }
+
+        return false;
+      }
+    }
+    throw new Error('Function not found');
   }
 }
